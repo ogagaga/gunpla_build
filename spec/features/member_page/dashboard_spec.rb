ce16a44a8a5702974523a81e806623377e1bfd2f
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2018-10-18 17:51:21 +0900

    Add maiking tools spec

--- spec/features/member_page/dashboard_spec.rb
+++ spec/features/member_page/dashboard_spec.rb
@@ -13,7 +13,7 @@ describe '個人会員ダッシュボード' do
       expect(page).to have_css('.dashboardHeader__title' ,text: 'Dashboard')
     end
 
-    it '製作管理一覧が表示できる' do
+    it '製作管理一覧のリンクが表示できる' do
       expect(page).to have_css('.dashboardSection__appMain' ,text: '製作道具管理')
     end
   end
--- spec/features/member_page/maiking_tools_spec.rb
+++ spec/features/member_page/maiking_tools_spec.rb
@@ -1,14 +1,15 @@
 describe '個人会員ダッシュボード' do
-  # let!(:member) { create(:xxx, :xxx, :xxx) }
+  let!(:member) { create(:alice) }
 
   context 'アカウントの登録が完了している場合' do
-    # before do
-    #   login_as member
-    #   visit member_page_root_path
-    # end
+    before do
+      login_as member
+      visit member_page_root_path
+    end
 
-    pending '製作管理一覧が表示できる'
-    # it '製作管理一覧が表示できる' do
-    # end
+    it '製作道具管理画面が表示できる' do
+      click_on '製作道具管理'
+      expect(page).to have_css('.makingtoolsHeader__title' ,text: '製作道具管理')
+    end
   end
 end
