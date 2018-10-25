f1c39983a0c214f715eb2acbbb90180ef9803ca6
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2018-10-25 17:44:22 +0900

    Add commit forgotten file

---
+++ spec/factories/making_tools.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :making_tool do
+    factory :design_knife do
+      name { 'デザインナイフ' }
+      price { 500 }
+      affiliate_amazon_link  { nil }
+      image { nil }
+      note { 'ゲート処理に必要です' }
+
+      association :making_tool_category, factory: :basic_tool, name: "基本道具"
+    end
+  end
+end
+
