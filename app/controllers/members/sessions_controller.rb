c42c7339470d144e4fab6ff94dda46e85113178d
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2018-10-26 11:40:10 +0900

    Fix member login design for navbar

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Members::SessionsController < Devise::SessionsController
+  layout 'member_page'
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
