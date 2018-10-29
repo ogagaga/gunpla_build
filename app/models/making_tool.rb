827428e6f924b079e5d568cbc31a161df6f654bc
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2018-10-29 16:47:46 +0900

    Fix members design
    - use bootstrap for simple_form
    - support responsive

--- app/models/making_tool.rb
+++ app/models/making_tool.rb
@@ -1,9 +1,10 @@
 class MakingTool < ApplicationRecord
   belongs_to :member
-  belongs_to :making_tool_category
+  belongs_to :making_tool_category ,optional: true
 
   mount_uploader :image, ImagesUploader
 
   validates :name, presence: true
   validates :making_tool_category_id, presence: true
+  validates :making_tool_category, presence: true, if: -> { making_tool_category_id.present? }
 end
