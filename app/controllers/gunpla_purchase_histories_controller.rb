bf90865741df8ce83a8d7c1e39f2053c44520905
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2018-11-07 23:02:06 +0900

    Fix pagination
    - 9 per page
    - fix previous desgin

--- app/controllers/gunpla_purchase_histories_controller.rb
+++ app/controllers/gunpla_purchase_histories_controller.rb
@@ -1,5 +1,5 @@
 class GunplaPurchaseHistoriesController < ApplicationController
-  PER_PAGE = 10
+  PER_PAGE = 9
   def index
     @gunpla_purchase_histories = GunplaPurchaseHistory.all.page(params[:page]).per(PER_PAGE)
   end
