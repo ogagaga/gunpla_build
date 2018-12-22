d48c54e800ad0818d2d70a040f94ffa07c01aad0
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2018-12-22 17:54:29 +0900

    Add create action for production_process

--- app/controllers/member_page/production_processes_controller.rb
+++ app/controllers/member_page/production_processes_controller.rb
@@ -12,6 +12,15 @@ class MemberPage::ProductionProcessesController < MemberPage::ApplicationControl
   end
 
   def create
+    ApplicationRecord.transaction do
+      @gunpla = current_member.gunplas.find(params[:gunpla_id])
+      @production_process = @gunpla.build_production_process(production_process_params)
+      @production_process.save!
+      redirect_to [:member_page, @gunpla, :production_process], notice: t('.notice')
+    end
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:alert] = t('.alert')
+    render :new
   end
 
   def edit
@@ -21,5 +30,16 @@ class MemberPage::ProductionProcessesController < MemberPage::ApplicationControl
   end
 
   def destroy
+  end
+
+  private
+
+  def production_process_params
+    params.require(:production_process).permit(
+      :title,
+      :summary,
+      :started_on,
+      :ended_on
+    )
   end
 end
